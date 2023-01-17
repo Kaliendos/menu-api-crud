38267792c7586be618244422828a1612f3bcf442
Владислав Кальендо <kaliendos@yahoo.com>
2023-01-17 16:35:28 +0300

    remove debug tolbar

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-from debug_toolbar.middleware import DebugToolbarMiddleware
 from fastapi import FastAPI
 
 from menu import router
 
+
 app = FastAPI()
-app.add_middleware(
-    DebugToolbarMiddleware,
-    panels=["debug_toolbar.panels.sqlalchemy.SQLAlchemyPanel"],
-    session_generators=["config.database.db:get_session"]
-)
+
 app.include_router(router)
-
-
-
-
